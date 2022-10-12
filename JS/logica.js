a9b27bab527d97a3f4f573f6ada629f2db60b175
VeroAbrego <114024927+VeckaAR@users.noreply.github.com>
2022-10-12 14:26:37 -0600

    add:js.

--- JS/logica.js
+++ JS/logica.js
@@ -5,31 +5,45 @@ function validar(){
     if (tipo.value==0){
             alert("Selecciona una opción para continuar");
             tipo.focus();
-        }else  if (tipo.value==1){
-            alert("Ingreso")
         }
         else{
-            alert("Egreso")
+            var descrip = document.getElementById("descripcion").value
+            var monto = document.getElementById("monto").value
+            if (descrip=="") {
+                var descrip = document.getElementById("descripcion").focus();
+            } else {
+                if (monto=="") {
+                    var descrip = document.getElementById("monto").focus();
+                } else {
+                    console.log(descrip+" "+monto);
+                    var descrip = document.getElementById("descripcion").value="";
+                    var monto = document.getElementById("monto").value="";
+                    var descrip = document.getElementById("descripcion").focus();
+                }
+            }
+
+            if (tipo.value==1){
+                alert("Ingreso")
+            }
+            else{
+                alert("Egreso")
+    
+            }
+
 
         }
+        
+        
+       
         
 }
 
 var getData = function(){
-    var descrip = document.getElementById("descripcion").value
-    var monto = document.getElementById("monto").value
-    if (descrip=="") {
-        var descrip = document.getElementById("descripcion").focus();
-    } else {
-        if (monto=="") {
-            var descrip = document.getElementById("monto").focus();
-        } else {
-            console.log(descrip+" "+monto);
-            var descrip = document.getElementById("descripcion").value="";
-            var monto = document.getElementById("monto").value="";
-            var descrip = document.getElementById("descripcion").focus();
-        }
-    }
+    
+   
     
 }
+
+
+
 
